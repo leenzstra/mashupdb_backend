567013ad3fe1c1a3c87266cf0bdc74b2dd6be2e2
leenzstra <leenzstra@gmail.com>
2024-02-07 20:07:46 +0500

    mdb: initial infra

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/leenzstra/mashupdb_backend
+
+go 1.22.0
---
+++ migrations/000001_create_mashup_table.up.sql
@@ -0,0 +1,6 @@
+CREATE TABLE IF NOT EXISTS mashup (
+    id bigserial,
+    author text,
+    name text,
+    link text
+);
